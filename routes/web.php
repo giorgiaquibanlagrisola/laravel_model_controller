b3f2bd60b7c6da330b462120e2e28be8d787baa3
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-03-01 17:21:30 +0100

    Inizio

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Controllers
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\Admin\BookController;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,18 +18,7 @@ use App\Http\Controllers\Admin\BookController;
 
 // Route::METODO(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
 
-Route::get('/', [MainController::class, 'index'])->name('home');   // <--- Che vantaggi ho nominando le rotte?
+Route::get('/', [PageController::class, 'index']);   // <--- Che vantaggi ho nominando le rotte?
 
-Route::get('/chi-siamo', [MainController::class, 'about'])->name('about');
+Route::get('/chi-siamo', [PageController::class, 'about']);
 
-/*
-    Questa istruzione definisce 7 rotte (in quest'ordine - perché è importante l'ordine delle rotte?):
-    - GET       /books                  -> books.index
-    - POST      /books                  -> books.store
-    - GET       /books/create           -> books.create
-    - GET       /books/{book}           -> books.show
-    - PUT       /books/{book}           -> books.update
-    - DELETE    /books/{book}           -> books.destroy
-    - GET       /books/{book}/edit      -> books.edit
-*/
-Route::resource('books', BookController::class);
